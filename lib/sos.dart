c84ce35105d8878d27fdebbf2a940371c6a1e62a
EmilxDaniel <emilsdaniel@gmail.com>
2024-04-25 17:36:12 +0530

    sos members already in the firestore will be fetched automaticaly  whenever the app opens up and the sos page will display those members as default

--- lib/sos.dart
+++ lib/sos.dart
@@ -4,6 +4,7 @@ import 'package:helmet/contacts.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:collection/collection.dart';
 
 class SOSMessagePage extends StatefulWidget {
   final List<String> selectedDisplayNames;
@@ -22,6 +23,48 @@ class _SOSMessagePageState extends State<SOSMessagePage> {
   final TextEditingController _messageController = TextEditingController();
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  List<String> savedDisplayNames = [];
+  List<String> savedPhoneNumbers = [];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _retrieveSOSData();
+  }
+
+  void _retrieveSOSData() async {
+    User? user = _auth.currentUser;
+    if (user != null) {
+      DocumentSnapshot<Map<String, dynamic>> snapshot =
+          await _firestore.collection('Users').doc(user.uid).get();
+      if (snapshot.exists) {
+        List<String> savedPhoneNumbers =
+            List<String>.from(snapshot.data()?['sos_contacts'] ?? []);
+        print(savedPhoneNumbers);
+
+        // Fetch display names corresponding to the selected phone numbers
+        List<Contact> contacts = await ContactsService.getContacts();
+        List<String> savedDisplayNames = [];
+        for (String phoneNumber in savedPhoneNumbers) {
+          Contact? contact = contacts.firstWhereOrNull((contact) =>
+              contact.phones!.any((phone) => phone.value == phoneNumber));
+          if (contact != null) {
+            savedDisplayNames.add(contact.displayName ?? 'Unknown');
+          } else {
+            savedDisplayNames.add('Unknown');
+          }
+        }
+        print(savedDisplayNames);
+
+        setState(() {
+          this.savedPhoneNumbers = savedPhoneNumbers;
+          this.savedDisplayNames = savedDisplayNames;
+        });
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -80,26 +123,52 @@ class _SOSMessagePageState extends State<SOSMessagePage> {
               ),
             ),
             SizedBox(height: 10),
-            Expanded(
-              child: ListView.builder(
-                itemCount: widget.selectedDisplayNames.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return ListTile(
-                    title: Text(widget.selectedDisplayNames[index]),
-                    subtitle: Text(widget.selectedPhoneNumbers[index]),
-                    trailing: IconButton(
-                      icon: Icon(Icons.delete),
-                      onPressed: () {
-                        setState(() {
-                          widget.selectedDisplayNames.removeAt(index);
-                          widget.selectedPhoneNumbers.removeAt(index);
-                        });
-                      },
-                    ),
-                  );
-                },
+            if ((savedDisplayNames.isEmpty && savedPhoneNumbers.isEmpty) ||
+                ((savedDisplayNames.isNotEmpty &&
+                        savedPhoneNumbers.isNotEmpty) &&
+                    (widget.selectedDisplayNames.isNotEmpty &&
+                        widget.selectedPhoneNumbers.isNotEmpty)))
+              Expanded(
+                child: ListView.builder(
+                  itemCount: widget.selectedDisplayNames.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return ListTile(
+                      title: Text(widget.selectedDisplayNames[index]),
+                      subtitle: Text(widget.selectedPhoneNumbers[index]),
+                      trailing: IconButton(
+                        icon: Icon(Icons.delete),
+                        onPressed: () {
+                          setState(() {
+                            widget.selectedDisplayNames.removeAt(index);
+                            widget.selectedPhoneNumbers.removeAt(index);
+                          });
+                        },
+                      ),
+                    );
+                  },
+                ),
+              )
+            else
+              Expanded(
+                child: ListView.builder(
+                  itemCount: savedDisplayNames.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return ListTile(
+                      title: Text(savedDisplayNames[index]),
+                      subtitle: Text(savedPhoneNumbers[index]),
+                      trailing: IconButton(
+                        icon: Icon(Icons.delete),
+                        onPressed: () {
+                          setState(() {
+                            savedDisplayNames.removeAt(index);
+                            savedPhoneNumbers.removeAt(index);
+                          });
+                        },
+                      ),
+                    );
+                  },
+                ),
               ),
-            ),
             SizedBox(height: 20),
             ElevatedButton(
               onPressed: () async {
@@ -131,10 +200,16 @@ class _SOSMessagePageState extends State<SOSMessagePage> {
     User? user = _auth.currentUser;
     if (user != null) {
       String messageContent = _messageController.text;
-      await _firestore.collection('Users').doc(user.uid).set({
-        'sos_contacts': widget.selectedPhoneNumbers,
-        'sos_message': messageContent,
-      }, SetOptions(merge: true));
+      if (widget.selectedPhoneNumbers.isEmpty)
+        await _firestore.collection('Users').doc(user.uid).set({
+          'sos_contacts': savedPhoneNumbers,
+          'sos_message': messageContent,
+        }, SetOptions(merge: true));
+      else
+        await _firestore.collection('Users').doc(user.uid).set({
+          'sos_contacts': widget.selectedPhoneNumbers,
+          'sos_message': messageContent,
+        }, SetOptions(merge: true));
       ScaffoldMessenger.of(context).showSnackBar(SnackBar(
         content: Text('SOS settings saved successfully'),
       ));
